99e4882bdb5de94790520d3773f5461f55967ab7
David Fozo <david.fozo@gmail.com>
2016-04-12 19:49:46 +0200

    Ch.4: Functional test now checks we can put to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
 		self.browser.quit()
@@ -16,18 +18,34 @@ class NewVisitorTest(unittest.TestCase):
 
 		#He notices that header and title mentions todo list
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		#He is invited to enter todo items straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a To-Do item'
+
+		)
 
 		#He types buy custom Cain rifle into a text box
+		inputbox.send_keys('buy custom Cain rifle')
 
 		#When he hits enter, the page updates, and now the page lists
 		#"1: buy custom Cain rifle" as an item in a to-do list
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows  = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: buy custom Cain rifle' for row in rows)
+
+		)
 
 		#There is still a text box inviting him to add another item. He
 		#enters "Sharpen blade"
-
+		self.fail('Finish the test!')
 		#The page updates again, and now shows both items on the list
 
 		#Mr.Cain wonders whether the site will remember his list. The he sees
