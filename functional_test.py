14a066887e8eef7938ac1a28ac8ddabcb1ae3cc8
David Fozo <david.fozo@gmail.com>
2016-04-13 13:47:53 +0200

    Ch.5: FT test for multiple list items. Home cannot do that right now.

--- functional_test.py
+++ functional_test.py
@@ -38,20 +38,26 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows  = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: buy custom Cain rifle' for row in rows),
-			"New to-do item did not appear in table."
-
-		)
+		self.assertIn('1: buy custom Cain rifle', [row.text for row in rows])
 
 		#There is still a text box inviting him to add another item. He
 		#enters "Sharpen blade"
-		self.fail('Finish the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('sharpen blade')
+		inputbox.send_keys(Keys.ENTER)
 		#The page updates again, and now shows both items on the list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows  = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: buy custom Cain rifle', [row.text for row in rows])
+		self.assertIn(
+			'2: sharpen blade',
+			[row.text for row in rows]
+		)
 
 		#Mr.Cain wonders whether the site will remember his list. The he sees
 		#that the site has generated a unique URL for him -- there is some
 		#explanatory text to that effect.
+		self.fail("Finish the test!")
 
 		#He visits that URL - his to-do list is still there.
 
--- lists/tests.py
+++ lists/tests.py
@@ -11,8 +11,22 @@ class HomePageTest(TestCase):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
 
-	def test_home_page_returns_correct_html(self):
+	#def test_home_page_returns_correct_html(self):
+	#	request = HttpRequest()
+	#	response = home_page(request)
+	#	expected_html = render_to_string('home.html')
+	#	self.assertEqual(response.content.decode(), expected_html)
+
+	def test_home_page_can_save_a_POST_request(self):
 		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_next'] = 'A new list item'
+
 		response = home_page(request)
-		expected_html = render_to_string('home.html')
+
+		self.assertIn('A new list item', response.content.decode())
+		expected_html = render_to_string(
+			'home.html',
+			{'new_item_text': 'A new list item'}
+		)
 		self.assertEqual(response.content.decode(), expected_html)
