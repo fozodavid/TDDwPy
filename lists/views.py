44b25e47dcac7b2f2af9444b09a00cee518b7615
David Fozo <david.fozo@gmail.com>
2016-04-14 15:52:32 +0200

    lists added as landing sites

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
@@ -12,5 +12,6 @@ def home_page(request):
 	# return render(request, 'home.html', {'items': items})
 
 def new_list(request):
-	Item.objects.create(text=request.POST.get('item_text',''))
+	list_ = List.objects.create()
+	Item.objects.create(text=request.POST.get('item_text',''), list=list_)
 	return redirect('/lists/the-only-list-in-the-world/')
