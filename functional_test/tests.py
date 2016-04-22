98d4522631afa5197c9d29c28c8221fe821b2c3e
David Fozo <david.fozo@gmail.com>
2016-04-22 17:14:47 +0200

    Ch.9: Add a fabfile for automated deploys, settings.py need to be rewritten after fresh deploy

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -2,8 +2,10 @@ import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from unittest import skip
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
 
 	@classmethod
 	def setUpClass(cls):
@@ -91,6 +93,23 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		#He visits that URL - his to-do list is still there.
 
 		#Satisfied, he goes back to sleep.
+
+	@skip
+	def test_cannot_add_empty_list_items(self):
+		# Mr.Cain goes to the home page and accidentally tries to submit
+		# an empty list item. She hits Enter on the empty input box
+
+		# The home page refreshes, and there is an error message saying 
+		# that list items cannot be blank
+
+		# She tries again with some text for the item, which now works
+
+		# Perversely, she now decides to submit a second blank list item
+
+		# She receives a similar warming on the list page
+
+		# And she can correct it by filling some text in
+		self.fail('write me!')
 
 if __name__ == '__main__':
 	unittest.main(warnings = 'ignore')
---
+++ superlists/secret_key.py
@@ -0,0 +1,1 @@
+SECRET_KEY = '+f&n8-p&5nnxdgb_k+8742on1&@)acp=7$evezuw-nyudcu(!='
